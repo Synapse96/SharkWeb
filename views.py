c1fddd7f370da8b16e4c7fc7cd6cea665bb20a68
Synapse96 <rajsharma96@hotmail.com>
2018-05-17 20:08:56 +1000

    intial views for mashup app

--- views.py
+++ views.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
+    # return high school locations for map or search bar from database of
+    # NSW High Schools
     return 'Hello World!'
+
+
+@app.route('/profile/<name>')
+def profile(name):
+    # given the name return the profile of the NSW High School if it exists
+    return 'NSW High School Profile'
 
 
 if __name__ == '__main__':
