924e223083ee961ecc9460efb082a2515beb2a65
Tony <tony.tran@outlook.com.au>
2018-05-18 10:14:25 +1000

    initial implementation of nsw school names and locations parsing

--- nsw_attendance_rates_data_pub.py
+++ nsw_attendance_rates_data_pub.py
@@ -2,6 +2,30 @@ from models import HighSchool
 import csv
 
 
+def parse_names_locations():
+    with open('static/datahub_nongov_locations-2017.csv') as csvfile:
+        rows = csv.reader(csvfile, delimiter=",")
+        for row in rows:
+            if "Secondary" in row[6]:
+                school = HighSchool.objects(id=row[0]) ## school's id isn't available in this data set
+                if school is not None:
+                    school.name = str(row[2])
+                    school.street = str(row[3])
+                    school.suburb = str(row[4])
+                    school.postcode = int(row[5])
+
+    with open('static/NSW-Public-Schools-Master-Dataset-07032017.csv') as csvfile:
+        rows = csv.reader(csvfile, delimiter=",")
+        for row in rows:
+            if "Secondary" in row[10]:
+                school = HighSchool.objects(id=row[0])
+                if school is not None:
+                    school.name = str(row[2])
+                    school.street = str(row[3])
+                    school.suburb = str(row[4])
+                    school.postcode = int(row[5])
+
+
 def parse_attendance_rates():
     with open('static/2011-2017-attendance-rates-by-nsw-government-schools.csv') as csvfile:
         rates = csv.reader(csvfile, delimiter=",")
