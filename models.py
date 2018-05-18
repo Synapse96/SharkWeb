967c162aa26887210c1e5ac834bb6c75a8b102fe
Tony <tony.tran@outlook.com.au>
2018-05-18 15:18:05 +1000

    initial implementation of nsw school names and locations parsing

--- models.py
+++ models.py
@@ -4,10 +4,16 @@ from mongoengine import Document, IntField, StringField, DictField
 class HighSchool(Document):
     id = IntField(required=True, primary_key=True)
     name = StringField(required=True)
+    street = StringField(required=True)
+    suburb = StringField(required=True)
+    postcode = IntField(required=True)
     attendance_rates = DictField(required=True)
 
-    def __init__(self, id, name, attendance_rates, *args, **kwargs):
+    def __init__(self, id, name, street, suburb, postcode, attendance_rates, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id = id
         self.name = name
+        self.street = street
+        self.suburb = suburb
+        self.postcode = postcode
         self.attendance_rates = attendance_rates
