eef6e3668469ca2bd6eef80027f93dfb202ec52a
Synapse96 <rajsharma96@hotmail.com>
2018-05-21 13:25:09 +1000

    updating attendance rates, added get method to retrieve attendance rates by year

--- nsw_attendance_rates_data_pub.py
+++ nsw_attendance_rates_data_pub.py
@@ -26,16 +26,29 @@ def parse_names_locations():
                     school.postcode = int(row[5])
 
 
+# Parses the attendance rates from the Data-set and places into the Schema
 def parse_attendance_rates():
+    # TODO: connect to mLab here
     with open('static/2011-2017-attendance-rates-by-nsw-government-schools.csv') as csvfile:
         rates = csv.reader(csvfile, delimiter=",")
         for row in rates:
             if "High School" in row[1]:
                 school = HighSchool.objects(id=row[0])
                 if school is not None:
-                    for i in range(0, 5):
-                        school.attendance_rate[str(2012 + i)] = row[i + 2]
+                    for i in range(0, 6):
+                        school.attendance_rate[str(2011 + i)] = row[i + 2]
 
 
-if __name__ == '__main__':
-    parse_attendance_rates()
+# Get the attendance rates (within [from_year,to_year] OPTIONAL)
+# for the given NSW HighSchool if it exists
+def get_attendance_rate(id, from_year=2011, to_year=2017):
+    # TODO: connect to mLab here
+    school = HighSchool.objects(id=id)
+    rates = []
+    if school is not None and from_year >= 2011 and to_year <= 2017:
+        rates = school.attendance_rate.values()
+        if from_year > 2011 or to_year < 2017:
+            rates = rates[from_year - 2012:to_year - 2012]
+    return rates
+
+
--- views.py
+++ views.py
@@ -5,14 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return high school locations for map or search bar from database of
-    # NSW High Schools
+    # TODO: return high school locations for map or search bar from database of NSW High Schools
     return 'Hello World!'
 
 
 @app.route('/profile/<name>')
 def profile(name):
-    # given the name return the profile of the NSW High School if it exists
+    # TODO: given the name return the profile of the NSW High School if it exists
     return 'NSW High School Profile'
 
 
