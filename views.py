35c45e92ec56bef1a99b384fd16283956196b371
Tony <tony.tran@outlook.com.au>
2018-05-27 13:47:08 +1000

    fixed small bug where schools with no photos caused an internal error

--- views.py
+++ views.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from mongoengine import connect
+import urllib.request
+import urllib.error
+import json
 
 app = Flask(__name__)
+
+connect(
+    host='mongodb://admin:comp9321@ds229450.mlab.com:29450/sharkweb'
+)
 
 
 @app.route('/')
@@ -10,12 +17,23 @@ def index():
     return 'Hello World!'
 
 
-@app.route('/profile/<name>')
-def profile(name):
-    # TODO: given the name return the profile of the NSW High School if it exists
-    return 'NSW High School Profile'
+@app.route('/profile/<id>')
+def profile(id):
+    response = {}
+    url_school = 'http://127.0.0.1:5001/school/' + id
+    url_photos = 'http://127.0.0.1:5002/photos/' + id
+    try:
+        with urllib.request.urlopen(url_school) as school_json:
+            school_data = json.load(school_json)
+            response = school_data
+        with urllib.request.urlopen(url_photos) as photos_json:
+            photos_data = json.load(photos_json)
+            response["photos"] = photos_data
+        return jsonify(response), 200
+    except urllib.error.HTTPError:
+        response["error"] = "invalid id in request"
+        return jsonify(response), 400
 
 
 if __name__ == '__main__':
-    connect(host='mongodb://admin:comp9321@ds229450.mlab.com:29450/sharkweb')
-    app.run()
+    app.run(port=5000)
