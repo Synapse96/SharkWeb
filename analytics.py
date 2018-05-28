891848958346693bcfcb1e131856c069761c08c9
Synapse96 <rajsharma96@hotmail.com>
2018-05-28 23:55:57 +1000

    small fix to compare

--- analytics.py
+++ analytics.py
@@ -84,7 +84,7 @@ def compare_schools():
             num_students[school.name] = int(school.students)
             if school.selective != 'Not Selective':
                 scores = list(map(float, school.selective_entry_scores.values()))
-                average_selective_entry = st.mean(scores)
+                average_selective_entry[school.name] = st.mean(scores)
 
     return jsonify({'attendances': average_attendances, 'enrollments': average_enrollments,
                     'students': num_students, 'selective_scores': average_selective_entry}), 200
